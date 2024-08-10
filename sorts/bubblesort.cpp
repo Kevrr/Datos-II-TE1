85ef8fdea4613e1867400edd62c28066b39edc09
Kevrr <krr284@gmail.com>
2024-08-09 19:32:06 -0600

    added sorts

---
+++ sorts/bubblesort.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+/**
+ * @brief algorithm to sort an array by checking two elements
+ * next to each other and swaping them if necesary, repeating 
+ * until no more swaps are necesary
+ * 
+ * @param arr array to sort
+ * @param n length of the array
+ */
+void bubbleSort(int arr[], int n) {
+    bool swapped;
+    for (int i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                std::swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        if (!swapped) {
+            break;
+        }
+    }
+}
